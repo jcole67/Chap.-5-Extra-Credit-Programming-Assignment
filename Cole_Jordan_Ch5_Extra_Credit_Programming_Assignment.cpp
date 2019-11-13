070faa7fbfb10f8f3ed9f66f2115137b3c5e4065
Jordan Cole <55559719+jcole67@users.noreply.github.com>
2019-11-13 15:17:01 -0800

    Create Cole_Jordan_Ch5_Extra_Credit_Programming_Assignment.cpp

---
+++ Cole_Jordan_Ch5_Extra_Credit_Programming_Assignment.cpp
@@ -0,0 +1,44 @@
+//Jordan Cole
+//CO SCI_802_26188FA19V
+//Chap. 5: Extra Credit Programming Assignment
+//Program that lets user enter a series of numbers, followed by -99 to signal the end of series.
+//Afterward, display largest and smallest numbers entered. 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Create variables for user input number, smallest number, and largest number
+    int num, smallest = 0, largest = 0;
+    
+    //Receive user input
+    cout << "Enter an integer (or -99 to quit): ";
+    cin >> num;
+    
+    //Input validation to allow the user to quit.
+    //Also, store largest and smallest numbers entered into correct variable.
+    while (num != -99)
+    {
+        cout << "Enter another integer (or -99 to quit): ";
+        cin >> num;
+        
+        if ((num >= largest) && (num != -99))
+        {
+           largest = num; 
+        }
+        if ((num <= smallest) && (num != -99))
+        {
+            smallest = num;
+        }
+        
+    }
+    
+    //Display the largest and smallest user inputs.
+    cout << endl;
+    cout << "The largest number you entered was " << largest << "." << endl;
+    cout << "The smallest number you entered was " << smallest << "." << endl;
+    
+    system("pause");
+    return 0;
+}
